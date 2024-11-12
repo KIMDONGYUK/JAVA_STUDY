cdacc5552458c8c4d919f6241be83cf706f2659e
KIMDONGYUK <kimdongyuk999@gmail.com>
2024-11-12 21:27:26 +0900

    2024.11.12 자바 수업

---
+++ 자바241112.java
@@ -0,0 +1,47 @@
+package java_study;
+
+// 자바 공부를 시작하겠습니다. (메모 <- 주석)
+public class 자바241112 {	// 자바는 한글 지원이 가능합니다.
+
+	public static void main(String[] args) { // 중괄호 시작
+		// 여기에 코드를 작성할 예정
+		// 문장 마지막에는 세미콜론을 작성한다.
+//		System.out.println("안녕하세요, 오늘 자바를 배웁니다.");
+		// Ctrl + F11을 동시에 누르면 JAVA가 실행됩니다.
+		// System.out.println : 작성한 내용을 확인할 수 있는 기능.
+		
+//		System.out.println("Hello World!");
+		
+		// syso -> Ctrl + space -> Enter (System.err.println(); 빠르게 타이핑)
+//		System.out.println("1");
+//		System.out.println("2");
+//		System.out.println("3");
+//		System.out.println("4");
+//		System.out.println("5");
+//		System.out.println("6");
+		// 코드 작성 후 -> Ctrl + s 눌러서 저장을 해줍니다. (이클립스는 자동 저장 아님)수시로 눌러주기.
+		
+//		System.out.println("현재 년도는 : " + 2024 + "입니다.");
+//		System.out.println(); : 출력문
+//		출력문에서 + 는 문자를 더해준다는 의미(문자와 문자를 합친다.)
+		
+//		System.out.println("키 : " + 172 + "입니다.");
+//		System.out.println("나이 : " + 27 + "입니다.");
+		// ln은 개행(줄바꿈)을 의마합니다.
+		// ln이 없으면 가로로 쭉 출력됩니다.
+//		System.out.print("안녕하세요 \n");
+//		System.out.print("안녕하세요 \n");
+		
+		// 변수 : 어떤 값을 대입할 때 사용.
+		// int는 숫자를 의미, String은 문자를 의미합니다.
+//		int age = 20;	// 나이
+//		String address = "대전 서구 둔산동";	// 주소
+//		String mbti = "ENFP";
+//		
+//		System.out.println("나이는 : " + age);	// age에 대입한 값이 출력됨
+//		System.out.println("주소는 : " + address);	// address에 대입한 값이 출력됨
+//		System.out.println("MBTI : " + mbti);
+		
+	}// 중괄호 끝
+
+}
